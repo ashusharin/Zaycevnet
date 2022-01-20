a04eec45f2bbd3143bacca8f958ccd876f8ea4ce
Alexandr Shusharin <ashusharin.dev@gmail.com>
2022-01-20 12:01:23 +0500

    fix Task

--- app/src/main/java/com/shusharin/zaycevnet/algorithms/Task.java
+++ app/src/main/java/com/shusharin/zaycevnet/algorithms/Task.java
@@ -8,17 +8,17 @@ public class Task {
 
     public @Nullable
     int[] decryptData(@NonNull int[] price,
-                      @IntRange(from = 1) int discount,
+                      @IntRange(from = 1, to = 100) int discount,
                       @IntRange(from = 0) int offset,
                       @IntRange(from = 1) int readLength) {
         int[] res = new int[readLength];
-        System.arraycopy(price,offset,res,0,readLength);
+        System.arraycopy(price, offset, res, 0, readLength);
         for (int i = 0; i < res.length; i++) {
-            double a =  res[i] * discount/100;
-            int b = (int) Math.floor(a);
-            res[i] = b;
+            double procent = (double) discount / 100;
+            double result = res[i] - (res[i]*procent);
+            int rounding =  (int)Math.floor(result);
+            res[i] = rounding;
         }
         return res;
     }
-
 }
